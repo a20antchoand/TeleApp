ec8a8b86ad74bbcf0fd967ab39504f35ba5017fc
a20pedlopcor <92155741+a20pedlopcor@users.noreply.github.com>
2022-03-31 01:07:49 +0200

    Pruebas menores (no conecta bien)

---
+++ src/main/java/es/lumsotf/cliente/ClienteProtocolo.java
@@ -0,0 +1,5 @@
+package es.lumsotf.cliente;
+
+public class ClienteProtocolo {
+
+}
---
+++ src/main/java/es/lumsotf/cliente/InicioChat.java
@@ -0,0 +1,7 @@
+package es.lumsotf.cliente;
+
+public class InicioChat {
+    public static void main(String[] args) {
+
+    }
+}
