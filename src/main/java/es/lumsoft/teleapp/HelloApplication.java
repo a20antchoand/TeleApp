0f6becf30ef77deef6cca1359c72363101d51a12
a20antchoand <a20antchoand@inspedralbes.cat>
2022-03-31 11:42:29 +0200

    Whatsapp visual

--- src/main/java/es/lumsoft/teleapp/HelloApplication.java
+++ src/main/java/es/lumsoft/teleapp/HelloApplication.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,61 +17,18 @@ import javafx.stage.Stage;
 
 public class HelloApplication extends Application{
 
-    private Pane root = new Pane();
-    private Scene scene;
-
-    private final Button add = new Button("Add");
-    private final VBox chatBox = new VBox(5);
-    private List<Label> messages = new ArrayList<>();
-    private ScrollPane container = new ScrollPane();
-    private int index = 0;
-
 
 
     @Override
     public void start(Stage stage) throws Exception{
 
-        initChatBox();
-        root.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        root.getChildren().addAll(container,add);
-        scene = new Scene(root,800,600);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("TeleApp");
         stage.setScene(scene);
         stage.show();
-
     }
 
-    private void initChatBox(){
-
-        container.setPrefSize(700, 400);
-        container.setContent(chatBox);
-
-        chatBox.getStyleClass().add("chatbox");
-
-        add.setOnAction(evt->{
-
-            messages.add(new Label("I'm a message"));
-
-            if(index%2==0){
-
-                messages.get(index).setAlignment(Pos.CENTER_LEFT);
-                System.out.println("1");
-
-            }else{
-
-                messages.get(index).setAlignment(Pos.CENTER_RIGHT);
-                System.out.println("2");
-
-            }
-
-
-            chatBox.getChildren().add(messages.get(index));
-            index++;
-
-        });
-
-
-
-    }
 
 
     public static void main(String[] args) {
--- src/main/java/es/lumsoft/teleapp/HelloController.java
+++ src/main/java/es/lumsoft/teleapp/HelloController.java
@@ -1,14 +1,69 @@
 package es.lumsoft.teleapp;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnEnviar;
+
+    @FXML
+    private Button btnRebre;
+
+    @FXML
+    private VBox containerMissatges;
+
+    @FXML
+    private TextArea textAreaMissatge;
+
+    @FXML
+    protected void enviarMissatge() {
+
+        HBox hbox = new HBox();
+
+        containerMissatges.setPadding(new Insets(10, 50, 50, 50));
+        containerMissatges.setSpacing(10);
+
+        hbox.setAlignment(Pos.BASELINE_RIGHT);
+
+        String text = textAreaMissatge.getText();
+        textAreaMissatge.setText("");
+
+        Label msg = new Label("Tú: " + text);
+        msg.setPadding(new Insets(10, 10, 10, 10));
+        msg.setStyle("-fx-background-color:WHITE");
+
+        hbox.getChildren().add(msg);
+        containerMissatges.getChildren().add(hbox);
+
     }
+
+    @FXML
+    protected void rebreMissatge() {
+
+        HBox hbox = new HBox();
+
+        containerMissatges.setPadding(new Insets(10, 50, 50, 50));
+        containerMissatges.setSpacing(10);
+
+        hbox.setAlignment(Pos.BASELINE_LEFT);
+
+        String text = textAreaMissatge.getText();
+        textAreaMissatge.setText("");
+
+        Label msg = new Label("Usuari: " + text);
+        msg.setPadding(new Insets(10, 10, 10, 10));
+        msg.setStyle("-fx-background-color:GREY");
+
+        hbox.getChildren().add(msg);
+        containerMissatges.getChildren().add(hbox);
+    }
+
 }
